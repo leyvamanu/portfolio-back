4bc7e858b42741366c0021d48148b2e188a3f168
Manu Leyva <leyvamanu@gmail.com>
2025-04-16 09:37:54 +0200

    Añadir favicon.ico

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->favicon(asset('favicon.ico'))
             ->colors([
                 'primary' => Color::Amber,
             ])
