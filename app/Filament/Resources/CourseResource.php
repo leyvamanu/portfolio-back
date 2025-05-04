6030ad9afd2fc04fd80c29a589a851120adfe77e
Manu Leyva <leyvamanu@gmail.com>
2025-05-04 12:57:49 +0200

    Añadir entidad para cursos

---
+++ app/Filament/Resources/CourseResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CourseResource\Pages;
+use App\Filament\Resources\CourseResource\RelationManagers;
+use App\Models\Course;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CourseResource extends Resource
+{
+    protected static ?string $model = Course::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-presentation-chart-bar';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->label('Nombre')->required()->maxLength(255),
+                TextInput::make('institution')->label('Centro')->required()->maxLength(255),
+                DatePicker::make('start_date')->label('Fecha de inicio')->required(),
+                DatePicker::make('end_date')->label('Fecha de finalización'),
+                TextInput::make('hours')->label('Cantidad de horas')->numeric()->minValue(1)->required(),
+                Textarea::make('description')->label('Descripción')->rows(4),
+                TextInput::make('url')->label('URL')->url()->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->label('Nombre')->searchable()->sortable(),
+                TextColumn::make('institution')->label('Centro'),
+                TextColumn::make('start_date')->label('Inicio')->date(),
+                TextColumn::make('end_date')->label('Fin')->date(),
+                TextColumn::make('hours')->label('Horas'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCourses::route('/'),
+            'create' => Pages\CreateCourse::route('/create'),
+            'edit' => Pages\EditCourse::route('/{record}/edit'),
+        ];
+    }
+}
