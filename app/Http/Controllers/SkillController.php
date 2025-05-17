a1f2aa7aeb956e75bed8aad32b34cfa84b95fc21
Manu Leyva <leyvamanu@gmail.com>
2025-05-17 10:59:57 +0200

    añadir endopoint para el listado de habilidades

---
+++ app/Http/Controllers/SkillController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\SkillResource;
+use App\Models\Skill;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class SkillController extends Controller
+{
+    public function index(): AnonymousResourceCollection
+    {
+        return SkillResource::collection(Skill::all()->sortByDesc('name'));
+    }
+}
